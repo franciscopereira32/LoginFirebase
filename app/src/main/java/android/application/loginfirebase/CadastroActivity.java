20aba1ef1800abe3858e0df2de6f0c11b413c1d0
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2022-08-04 16:37:13 -0300

    started MainActivity and CadasttroActivity

--- app/src/main/java/android/application/loginfirebase/CadastroActivity.java
+++ app/src/main/java/android/application/loginfirebase/CadastroActivity.java
@@ -3,12 +3,40 @@ package android.application.loginfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText edNome;
+    private EditText edEmail;
+    private EditText edSenha;
+    private Switch swProfessor;
+    private Button btCadastrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        edNome = findViewById(R.id.edNome);
+        edEmail = findViewById(R.id.edEmail);
+        edSenha = findViewById(R.id.edSenha);
+        swProfessor = findViewById(R.id.swProfessor);
+        btCadastrar = findViewById(R.id.btCadastrar);
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recuperarDados();
+            }
+        });
+    }
+
+    private void recuperarDados() {
+
     }
 }
